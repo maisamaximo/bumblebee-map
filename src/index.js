de80c77a3b648296780c4d2926704df30f02ee1a
maisamaximo <ferreira.maximo.maisa@gmail.com>
2025-07-11 22:42:06 +0100

    first commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-import React from 'react';
-import { createRoot } from 'react-dom/client';  // ou ReactDOM.render, dependendo da versão
-import './index.css';
-import App from './App';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
